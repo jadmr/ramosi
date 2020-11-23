4723d42b0b6211991db5eecf7c77ac8924a39e5a
JAD <programming@coffeemark.us>
2020-11-23 05:23:51 -0800

    Add plant models

---
+++ Data/Entities/Plant.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Entities
+{
+    public class Plant
+    {
+        public Plant()
+        {
+            Aliases = new List<PlantAlias>(){};
+        }
+
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; }
+
+        public string Notes { get; set; }
+
+        public virtual IList<PlantAlias> Aliases { get; set; }
+    }
+}
---
+++ Data/Entities/PlantAlias.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Entities
+{
+    public class PlantAlias
+    {
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual Plant Plant { get; set; }
+    }
+}
