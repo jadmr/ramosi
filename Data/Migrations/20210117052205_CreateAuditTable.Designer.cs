365ee92e4bd24d01914f8de17772a62e5f8f6d0e
JAD <programming@coffeemark.us>
2021-01-16 21:26:10 -0800

    Add migration

--- Data/Migrations/20210117052205_CreateAuditTable.Designer.cs
+++ Data/Migrations/20210117052205_CreateAuditTable.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(RamosiContext))]
-    partial class RamosiContextModelSnapshot : ModelSnapshot
+    [Migration("20210117052205_CreateAuditTable")]
+    partial class CreateAuditTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20210117052205_CreateAuditTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateAuditTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Audit",
+                columns: table => new
+                {
+                    Guid = table.Column<byte[]>(nullable: false),
+                    EntityGuid = table.Column<byte[]>(nullable: false),
+                    ChangeType = table.Column<string>(nullable: false),
+                    OldValue = table.Column<string>(nullable: true),
+                    NewValue = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTimeOffset>(nullable: false),
+                    UserId = table.Column<byte[]>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audit", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_Audit_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Audit_UserId",
+                table: "Audit",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audit");
+        }
+    }
+}
